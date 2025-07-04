f46a4ffe56f1cbc4a751791f819dc96f742273a9
yesh21 <yaswanth.pulavarthi@gmail.com>
2025-07-04 16:29:25 +0530

    added HttpGraphQlTester for IT

--- src/test/java/com/recordsapi/graphql/HelloDataFetcherIntegrationTest.java
+++ src/test/java/com/recordsapi/graphql/HelloDataFetcherIntegrationTest.java
@@ -1,28 +1,43 @@
 package com.recordsapi.graphql;
 
-import com.netflix.graphql.dgs.DgsQueryExecutor;
-import org.intellij.lang.annotations.Language;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.graphql.test.tester.HttpGraphQlTester;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class HelloDataFetcherIntegrationTest {
 
-    @Autowired
-    DgsQueryExecutor dgsQueryExecutor;
+    private HttpGraphQlTester graphQlTester;
+
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    void setUp() {
+        WebTestClient client = WebTestClient.bindToServer()
+                .baseUrl("http://localhost:" + port + "/graphql")
+                .build();
+        graphQlTester = HttpGraphQlTester.create(client);
+    }
 
     @Test
     void helloQuery_ReturnsGreeting() {
-        @Language("GraphQL")
-        String query = "query { hello }";
-        String result = dgsQueryExecutor.executeAndExtractJsonPath(
-                query,
-                "data.hello"
-        );
-        assertThat(result).isEqualTo("Hello, Intern!");
+        // language=GraphQL
+        String query = """
+            query {
+                hello
+            }
+            """;
+
+        graphQlTester.document(query)
+                .execute()
+                .path("hello")
+                .entity(String.class)
+                .satisfies(result -> assertThat(result).isEqualTo("Hello, Intern!"));
     }
 }
-
